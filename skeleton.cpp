0b96d31938b5aa9283f2b3b549231054a317063e
roroal23 <rodrigo.najar.portaro@gmail.com>
2025-12-29 18:25:07 +0100

    Added a solution skeleton

---
+++ skeleton.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/*
+    Problem XXX: Description
+    Difficulty: Easy
+    Time complexity: O(n), where n is
+    Space complexity: O(1)
+
+    Approach:
+*/
+
+class Solution {
+public:
+};
+
+/*Test*/
+int main()
+{
+    Solution sol;
+}
